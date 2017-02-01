0a2314426cd9189812111badaf861d8f6998c311
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2017-02-01 16:44:36 -0200

    Implementacao da estrutura de dados

--- AcionamentoRadio/AcionamentoRadio.ino
+++ AcionamentoRadio/AcionamentoRadio.ino
@@ -2,22 +2,36 @@
  *Acionamento de lampada via radio frequencia 
  *Autor Natan 
  */
+#define Olamada 13
+#define Otomada 12
+#define Oled 11
+#define Ilampada 0
+#define Itomada 1
+#define Iled 3
+#define Isw0 4
+#define Isw1 5
 
-int *valor;
+#define DEBUG
 
-void updateOI (int *valor);
+struct valor{
+    int estado  : 1;
+    int estado1 : 1;
+  };
+typedef struct valor valor_t;
+
+void updateIO (valor_t *valor);
 
 void setup() {
   // put your setup code here, to run once:
+
+#ifdef DEBUG
+Serial.begin (9600);
+Serial.println("Config");
+#endif //DEBUG
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
-
-void updateIO (int *valor){
-  
-  
 }
