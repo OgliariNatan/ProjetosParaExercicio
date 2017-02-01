b561d86fdab302a2c6990515af2fb010c8f9927c
OgliariNatan <natan.o@aluno.ifsc.edu.br>
2017-02-01 15:51:29 -0200

    automacao quarto via radio [Hz]

---
+++ AcionamentoRadio/AcionamentoRadio.ino
@@ -0,0 +1,23 @@
+/*
+ *Acionamento de lampada via radio frequencia 
+ *Autor Natan 
+ */
+
+int *valor;
+
+void updateOI (int *valor);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void updateIO (int *valor){
+  
+  
+}
