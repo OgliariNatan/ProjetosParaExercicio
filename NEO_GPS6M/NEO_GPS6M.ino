de72a4bf490e7da0b8a8e8afb6aa15b7b6d2c243
OgliariNatan <natanogliari@gmail.com>
2017-07-20 15:25:28 -0300

    GPS/instrução de dados

--- NEO_GPS6M/NEO_GPS6M.ino
+++ NEO_GPS6M/NEO_GPS6M.ino
@@ -4,14 +4,28 @@
 //    3    ||    RX
 //   GND   ||   GND
 
+/*Instruções de dados:
+ * $GPRMC,044235.000,A,4322.0289,N,00824.5210,W,0.39,65.46,020615,,,A*44
+ * 
+ * - 044235.000 representa a hora GMT (04:42:35)
+ * - “A” es la indicación de que el dato de posición está fijado y es correcto. “V” sería no válido
+ * - 4322.0289 representa la longitud (43º 22.0289´)
+ * - N representa el Norte
+ * - 00824.5210 representa la latitud (8º 24.5210´)
+ * - W representa el Oeste
+ * - 0.39 representa la velocidad en nudos
+ * - 65.46 representa la orientación en grados
+ * - 020615 representa la fecha (2 de Junio del 2015)
+ */
+
+
 
 #include <SoftwareSerial.h>
 SoftwareSerial gps(4,3);//RX,TX
 
-char dato=' ';
+char data=' ';
 
-void setup()
-{
+void setup(){
  Serial.begin(115200);            
  gps.begin(9600); 
 }
@@ -19,9 +33,8 @@ void setup()
 
 void loop()
 {
-  if(gps.available())
-  {
-    dato=gps.read();
-    Serial.print(dato);
+  if(gps.available()){
+    data=gps.read();
+    Serial.print(data);
   }
 }
